20bb200a4925dc589329d665e5879c4836ac8ce2
Richard Green <richardgreen@pursuit.org>
2019-02-12 02:49:17 -0500

    rendered the image from the search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import LoadMore from './components/loadMore'
-import SearchBar from './components/SearchBar'
+import { SearchBar, Bar} from './components/SearchBar'
 class App extends Component {
   constructor(props){
    super(props)
@@ -13,13 +13,26 @@ class App extends Component {
    this.setState({pokemons:newPokemonArr});
   console.log(this.state);
  }
+
+ searchPokemon = pokemon => {
+  //  this.state.pokemons.map((e)=>{
+  //     if(e !== pokemon){
+  //       return 'Enter a Pokemon'
+  //     }
+  //     return e
+  //  })
+  this.setState({pokemons:pokemon})
+  console.log(this.state);
+ }
  
   render() {
+    let poke = this.state.pokemons
     return (
       <>
       <div className="App">
       <div className ='container'>
-        <SearchBar name={'Ivysaur'}/>
+        <Bar searchPokemon={this.searchPokemon}/>
+        <SearchBar poke={poke} />
         <LoadMore getPokemon={this.getPokemon} pokemons={this.state.pokemons} />
       </div>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,18 +3,23 @@ import Axios from 'axios'
 import PokemonList from './PokemonList'
 
 // console.log(PokemonList.Charmander)
+
+
 const SearchBar = (props) => {
-    if(props.name !== PokemonList[`${props.name}`]){
+    
+    console.log(props.poke)
+    console.log('2',PokemonList[`${props.poke}`])
+    if(props.poke !== PokemonList[`${props.poke}`]){
         return 'Not a Valid Pokemon'
     }
     
-    return <div >
+    return <div>
     <div className = 'row'>
         <div className = 'col-4'>
-            <img src={`https://img.pokemondb.net/sprites/sun-moon/icon/${PokemonList[`${props.name}`].toLocaleLowerCase()}.png`}alt={`${PokemonList[`${props.name}`]}`}/>
+            <img src={`https://img.pokemondb.net/sprites/sun-moon/icon/${PokemonList[`${props.poke}`].toLocaleLowerCase()}.png`}alt={`${PokemonList[`${props.poke}`]}`}/>
         </div>
         <div className = 'col-4'>
-            <p>{PokemonList[`${props.name}`]}</p>
+            <p>{PokemonList[`${props.poke}`]}</p>
         </div>
         </div>
     </div>
@@ -22,4 +27,14 @@ const SearchBar = (props) => {
 
 }
 
-export default SearchBar
+const Bar = (props) =>{
+  
+    return <>
+    <form className="form-inline" >
+      <input className="form-control mr-ls-2" type="text" placeholder="Search..." aria-label="lol" onChange={(e=>{props.searchPokemon(e.currentTarget.value)})} />
+    </form>
+    
+  </>
+}
+// SearchBar(e.target.value)
+export {SearchBar, Bar}
