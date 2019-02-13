0bce2da0fdfc7cf2bf7c1b2c3d35be2d6844a300
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-13 18:59:46 -0500

    Formatting changes for title

--- src/App.js
+++ src/App.js
@@ -39,13 +39,14 @@ class App extends Component {
       <div className='row'>
       <div className=' col-4' >
       <img className='pokeball' src='https://files.slack.com/files-pri/TD416AWAE-FG4T6BAHH/pokeball.png' alt='lol' />
-
       </div>
-      <div className='col-8'>
+      <div className='col-4'>
+      <h1> Pursuit Pokedex </h1>
+      </div>
+      <div className='col-4'>
       <img className='pokeball' src='https://files.slack.com/files-pri/TD416AWAE-FG4T6BAHH/pokeball.png' alt='lol' />
 
  </div>
-      <h1>Pursuit Pokedex</h1>
 
       </div>
       <img></img>
