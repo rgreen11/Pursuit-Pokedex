88f56230574a3c2bede6cf8df6ff40908ab54371
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-18 23:37:15 -0500

    style changes to app

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = props => {
     return (
-        <div className='row'>
+        <div className='row header'>
         <div className=' col-4' >
         <img className='pokeball' src='https://ih1.redbubble.net/image.154435705.5064/ap,550x550,12x12,1,transparent,t.u2.png' alt='lol' />
         </div>
