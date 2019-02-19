6554e1de0a00efd2713c6f55960a2fd752d2abd7
Jorgebillini <Jorgebillini@pursuit.org>
2019-02-19 14:08:20 -0500

    Fixed search bar bug that would not allow to search for pokemon and throw error

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
 
 
     PokeSearch = (e) => {
+        console.log(e.target.value)
         if(this.state.search.includes(' ')){
             this.setState({search:[]})
         } else if(this.state.search.includes(e.target.value)){
@@ -26,8 +27,10 @@ class SearchBar extends React.Component {
 
           }
     ProfileClick = (e) => {
-        console.log(e);
-        this.props.handleProfileClick(e)
+        if (this.state.search.includes(e.target.value)){
+            this.props.handleProfileClick(e.target.value)
+
+        }
     }    
           
     PokeClick = (e) => {
@@ -57,7 +60,7 @@ class SearchBar extends React.Component {
             <div className='row'>
             <div className='col search-container'>
             <div className="Search-box">
-                <input type="text" className="searchText" placeholder ='PokeBallers...' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" list = 'pokemonList' onChange ={this.PokeSearch} />
+                <input type="text" className="searchText" placeholder ='PokeBallers...' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" list = 'pokemonList' onChange ={this.PokeSearch} onInput={this.ProfileClick}/>
                     <a className ='searchButton' href ='ok'>
                          <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/0c3bbade-58ef-4c64-9a3e-26cf3c477338/d7ea28n-430864db-4a85-4973-84ff-3b494a72beec.png/v1/fill/w_898,h_890,strp/master_ball__01__by_adfpf1_d7ea28n-pre.png
                         ' alt ='' height ='60px' className="img"/>  
