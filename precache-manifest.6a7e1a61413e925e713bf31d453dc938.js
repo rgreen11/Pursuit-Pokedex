19085c40bf76033bf9f6844b5b6812aa63573819
Richard Green <richardgreen@pursuit.org>
2019-03-24 16:36:55 -0400

    Updates

---
+++ precache-manifest.6a7e1a61413e925e713bf31d453dc938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b41d46d9f1146c733924",
+    "url": "/static/js/main.b41d46d9.chunk.js"
+  },
+  {
+    "revision": "61028ae5f1be85ff390f",
+    "url": "/static/js/1.61028ae5.chunk.js"
+  },
+  {
+    "revision": "b41d46d9f1146c733924",
+    "url": "/static/css/main.2e647e65.chunk.css"
+  },
+  {
+    "revision": "61028ae5f1be85ff390f",
+    "url": "/static/css/1.ec9dec15.chunk.css"
+  },
+  {
+    "revision": "c7ff3a4ba25b48c9dd2d8b6ac3f04d54",
+    "url": "/index.html"
+  }
+];
